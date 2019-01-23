a6b25a3e59f68f38834beb00f82d0bcea03e8400
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-01-23 11:05:55 -0700

    Created author table in SQL.

---
+++ sql/author.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE author(
+	authorId binary(16) 	NOT NULL,
+	authorAvatarUrl 		VARCHAR(255),
+	authorActivationToken CHAR(32),
+	authorEmail 			VARCHAR(128) NOT NULL,
+	authorHash 				CHAR(97) NOT NULL,
+	authorUsername 		VARCHAR(32) NOT NULL,
+		UNIQUE(authorEmail),
+		UNIQUE(authorUsername),
+		INDEX(authorEmail),
+		PRIMARY KEY(authorId)
+);
+
+SELECT * FROM author;
